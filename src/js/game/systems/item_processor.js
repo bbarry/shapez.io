1864490e664fc0390c04419d4e2282981983ab89
Sense101 <67970865+Sense101@users.noreply.github.com>
2020-11-13 10:51:45 +0000

    Update item_processor.js

--- src/js/game/systems/item_processor.js
+++ src/js/game/systems/item_processor.js
@@ -156,14 +156,6 @@ export class ItemProcessorSystem extends GameSystemWithFilter {
         }
     }
 
-            // Check if we have an empty queue and can start a new charge
-            if (processorComp.ongoingCharges.length < MAX_QUEUED_CHARGES) {
-                if (this.canProcess(entity)) {
-                    this.startNewCharge(entity);
-                }
-            }
-        }
-    }
 
     /**
      * Returns true if the entity should accept the given item on the given slot.
