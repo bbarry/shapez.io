9b3e784c441c0475df52d41ee25905e9a6342310
Sense101 <67970865+Sense101@users.noreply.github.com>
2020-11-15 17:55:24 +0000

    Update belt.js

--- src/js/game/buildings/belt.js
+++ src/js/game/buildings/belt.js
@@ -9,7 +9,7 @@ import { MetaBuilding } from "../meta_building";
 import { GameRoot } from "../root";
 import { THEME } from "../theme";
 
-export const arrayBeltVariantToRotation = [enumDirection.top, enumDirection.left, enumDirection.right];
+export const arrayBeltVariantToRotation = [enumDirection.top/*, enumDirection.left, enumDirection.right*/];
 
 export const beltOverlayMatrices = {
     [enumDirection.top]: generateMatrixRotations([0, 1, 0, 0, 1, 0, 0, 1, 0]),
