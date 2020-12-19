bf316d7ebc83cc556e85cf76bb885243901e0fe6
Sense101 <67970865+Sense101@users.noreply.github.com>
2020-12-19 23:02:36 +0000

    k

--- src/js/game/systems/item_processor.js
+++ src/js/game/systems/item_processor.js
@@ -438,11 +438,14 @@ export class ItemProcessorSystem extends GameSystemWithFilter {
         const nextSlot = processorComp.nextOutputSlot++ % availableSlots;
 
         for (let i = 0; i < payload.items.length; ++i) {
-            payload.outItems.push({
-                item: payload.items[i].item,
-                preferredSlot: (nextSlot + i) % availableSlots,
-                doNotTrack: true,
-            });
+            if(!(payload.items.length <= 0))
+            {
+                payload.outItems.push({
+                    item: payload.items[i].item,
+                    preferredSlot: (nextSlot + i) % availableSlots,
+                    doNotTrack: true,
+                });
+            }
         }
         return true;
     }
