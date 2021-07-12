129008d2b511b4471e9e6121d680f0f57f09e24c
TcePrepK <56453014+TcePrepK@users.noreply.github.com>
2021-07-12 19:03:50 +0300

    commithash

--- src/js/core/query_parameters.js
+++ src/js/core/query_parameters.js
@@ -3,20 +3,20 @@ const options = queryString.parse(location.search);
 
 export let queryParamOptions = {
     embedProvider: null,
-    fullVersion: false,
-    sandboxMode: false,
+    fullVersion: true,
+    sandboxMode: true,
 };
 
 if (options.embed) {
     queryParamOptions.embedProvider = options.embed;
 }
 
-// Allow testing full version outside of standalone
-if (options.fullVersion && !G_IS_RELEASE) {
-    queryParamOptions.fullVersion = true;
-}
+// // Allow testing full version outside of standalone
+// if (options.fullVersion && !G_IS_RELEASE) {
+//     queryParamOptions.fullVersion = true;
+// }
 
-// Allow testing full version outside of standalone
-if (options.sandboxMode && !G_IS_RELEASE) {
-    queryParamOptions.sandboxMode = true;
-}
+// // Allow testing full version outside of standalone
+// if (options.sandboxMode && !G_IS_RELEASE) {
+//     queryParamOptions.sandboxMode = true;
+// }
